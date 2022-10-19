86a3569db67a298627a2d4804a09f25317babc4d
neutral-jungle-creep <arrrseny11@gmail.com>
2022-10-19 23:50:15 +0300

    edit user.go in usecase

--- internal/usecase/user.go
+++ internal/usecase/user.go
@@ -3,10 +3,18 @@ package usecase
 import (
 	"bot_logger/configs"
 	"bot_logger/internal/domain"
+	"bot_logger/internal/storage/pgSQL"
 )
 
-func RunUser(user *domain.User, config configs.Configuration) {
+func RunUser(user *domain.User, config *configs.Configuration) *pgSQL.WriteDBResult {
+	conn, err := pgSQL.NewConnectToDataBase(config)
+	if err != nil {
+		return &pgSQL.WriteDBResult{Err: err}
+	}
 
+	result := pgSQL.WriteUserToDB(user, conn)
+
+	return &pgSQL.WriteDBResult{Err: result}
 }
 
 func NewUser(name string, id string, active bool) *domain.User {

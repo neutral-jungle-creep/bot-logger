08b9bf73efa3ee5f488b8c792a7cbc8e52d3865a
neutral-jungle-creep <arrrseny11@gmail.com>
2022-10-24 00:47:24 +0300

    add storage user.go

---
+++ internal/storage/user.go
@@ -0,0 +1,48 @@
+package storage
+
+import (
+	"bot_logger/internal/domain"
+	"context"
+	"github.com/jackc/pgx/v4"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+type PgUserStorage struct {
+	conn *pgx.Conn
+}
+
+func NewPgUserStorage(conn *pgx.Conn) *PgUserStorage {
+	return &PgUserStorage{
+		conn: conn,
+	}
+}
+
+func (s *PgUserStorage) AddUser(user *domain.User) error {
+	_, err := s.conn.Exec(context.Background(), viper.GetString("queries.addUser"),
+		user.Id,
+		user.Username,
+		true,
+	)
+
+	if err != nil {
+		logrus.Printf("Ошибка добавления пользователя в базу данных: %s", err.Error())
+		return err
+	}
+	logrus.Printf("Добавление нового пользователя в базу данных: %v", *user)
+	return nil
+}
+
+func (s *PgUserStorage) EditUser(user *domain.User) error {
+	_, err := s.conn.Exec(context.Background(), viper.GetString("queries.editUser"),
+		false,
+		user.Id,
+	)
+
+	if err != nil {
+		logrus.Printf("Ошибка редактирования пользователя в базе данных: %s", err.Error())
+		return err
+	}
+	logrus.Printf("Редактирование пользователя в базе данных: %v", *user)
+	return nil
+}

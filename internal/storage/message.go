4c7fb7c82385f5e23a6ad198eb2fa9d80f6690cd
neutral-jungle-creep <arrrseny11@gmail.com>
2022-10-24 00:54:43 +0300

    add storage message.go

---
+++ internal/storage/message.go
@@ -0,0 +1,51 @@
+package storage
+
+import (
+	"bot_logger/internal/domain"
+	"context"
+	"github.com/jackc/pgx/v4"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/viper"
+)
+
+type PgMessageStorage struct {
+	db *pgx.Conn
+}
+
+func NewPgMessageStorage(db *pgx.Conn) *PgMessageStorage {
+	return &PgMessageStorage{
+		db: db,
+	}
+}
+
+func (s *PgUserStorage) AddMessage(message *domain.Message) error {
+	_, err := s.conn.Exec(context.Background(), viper.GetString("queries.addMessage"),
+		message.Id,
+		message.Date,
+		message.Text,
+		false,
+		message.SenderId,
+	)
+
+	if err != nil {
+		logrus.Printf("Ошибка добавления сообщения в базу данных: %s", err.Error())
+		return err
+	}
+	logrus.Printf("Добавление нового сообщения в базу данных: %v", *message)
+	return nil
+}
+
+func (s *PgUserStorage) EditMessage(message *domain.Message) error {
+	_, err := s.conn.Exec(context.Background(), viper.GetString("queries.editMessage"),
+		message.Text,
+		true,
+		message.Id,
+	)
+
+	if err != nil {
+		logrus.Printf("Ошибка редактирования сообщения в базе данных: %s", err.Error())
+		return err
+	}
+	logrus.Printf("Редактирование сообщения в базе данных: %v", *message)
+	return nil
+}

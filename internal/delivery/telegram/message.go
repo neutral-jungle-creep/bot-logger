5c5050f72760f8100255e576c8070eb858c663c8
neutral-jungle-creep <arrrseny11@gmail.com>
2022-10-26 22:49:34 +0300

    fix user.go & message.go

--- internal/delivery/telegram/message.go
+++ internal/delivery/telegram/message.go
@@ -5,6 +5,7 @@ import (
 	"bot_logger/internal/service/dto"
 	"bot_logger/internal/storage"
 	"bot_logger/internal/storage/pgSQL"
+	"context"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/sirupsen/logrus"
 	"strconv"
@@ -12,39 +13,35 @@ import (
 )
 
 func (h *Handler) AddMessage(message *tgbotapi.Message) error {
-	messageService, err := messageComposite()
+	conn, err := pgSQL.NewPgConnect()
 	if err != nil {
+		logrus.Infof("Ошибка подключения к базе данных: %s", err.Error())
 		return err
 	}
+	defer conn.Close(context.Background())
 
+	stor := storage.NewPgMessageStorage(conn)
+	serv := service.NewMessageService(stor)
 	messageDto := dto.NewMessageDto(message.MessageID, message.From.ID, parseTimeStamp(message.Date),
 		message.Text, false)
-	result := messageService.AddMessage(messageDto)
+	result := serv.AddMessage(messageDto)
 	return result
 }
 
 func (h *Handler) EditMessage(message *tgbotapi.Message) error {
-	messageService, err := messageComposite()
-	if err != nil {
-		return err
-	}
-
-	messageDto := dto.NewMessageDto(message.MessageID, message.From.ID, parseTimeStamp(message.Date),
-		message.Text, true)
-	result := messageService.EditMessage(messageDto)
-	return result
-}
-
-func messageComposite() (*service.MessageService, error) {
 	conn, err := pgSQL.NewPgConnect()
 	if err != nil {
 		logrus.Infof("Ошибка подключения к базе данных: %s", err.Error())
-		return nil, err
+		return err
 	}
+	defer conn.Close(context.Background())
 
 	stor := storage.NewPgMessageStorage(conn)
 	serv := service.NewMessageService(stor)
-	return &serv, nil
+	messageDto := dto.NewMessageDto(message.MessageID, message.From.ID, parseTimeStamp(message.Date),
+		message.Text, true)
+	result := serv.EditMessage(messageDto)
+	return result
 }
 
 func parseTimeStamp(timeStamp int) string {
--- internal/delivery/telegram/user.go
+++ internal/delivery/telegram/user.go
@@ -5,40 +5,37 @@ import (
 	"bot_logger/internal/service/dto"
 	"bot_logger/internal/storage"
 	"bot_logger/internal/storage/pgSQL"
+	"context"
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 	"github.com/sirupsen/logrus"
 )
 
 func (h *Handler) AddUser(user *tgbotapi.User) error {
-	userService, err := userComposite()
+	conn, err := pgSQL.NewPgConnect()
 	if err != nil {
+		logrus.Infof("Ошибка подключения к базе данных: %s", err.Error())
 		return err
 	}
+	defer conn.Close(context.Background())
 
+	stor := storage.NewPgUserStorage(conn)
+	serv := service.NewUserService(stor)
 	userDto := dto.NewUserDto(user.ID, user.UserName, true)
-	result := userService.AddUser(userDto)
+	result := serv.AddUser(userDto)
 	return result
 }
 
 func (h *Handler) EditUser(user *tgbotapi.User) error {
-	userService, err := userComposite()
-	if err != nil {
-		return err
-	}
-
-	userDto := dto.NewUserDto(user.ID, user.UserName, false)
-	result := userService.EditUser(userDto)
-	return result
-}
-
-func userComposite() (*service.UserService, error) {
 	conn, err := pgSQL.NewPgConnect()
 	if err != nil {
 		logrus.Infof("Ошибка подключения к базе данных: %s", err.Error())
-		return nil, err
+		return err
 	}
+	defer conn.Close(context.Background())
 
 	stor := storage.NewPgUserStorage(conn)
 	serv := service.NewUserService(stor)
-	return &serv, nil
+	userDto := dto.NewUserDto(user.ID, user.UserName, false)
+	result := serv.EditUser(userDto)
+	return result
 }

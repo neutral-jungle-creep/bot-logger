07b7bf7438335664661e8e297ee82d64093887fb
neutral-jungle-creep <arrrseny11@gmail.com>
2022-10-30 22:25:12 +0300

    add storage.go

---
+++ internal/storage/storage.go
@@ -0,0 +1,29 @@
+package storage
+
+import (
+	"bot_logger/internal/domain"
+	"github.com/jackc/pgx/v4"
+)
+
+type User interface {
+	GetUser(user *domain.User) error
+	AddUser(user *domain.User) error
+	EditUser(user *domain.User) error
+}
+
+type Message interface {
+	AddMessage(message *domain.Message) error
+	EditMessage(message *domain.Message) error
+}
+
+type Storage struct {
+	User
+	Message
+}
+
+func NewStorage(conn *pgx.Conn) *Storage {
+	return &Storage{
+		User:    NewPgUserStorage(conn),
+		Message: NewPgMessageStorage(conn),
+	}
+}
